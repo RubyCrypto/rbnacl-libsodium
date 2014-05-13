f88820e58cf385beaa03dfd09d405daf4eeeb485
Tony Arcieri <bascule@gmail.com>
2014-05-13 09:28:45 -0700

    Bump version to 0.4.5a

---
+++ CHANGES.md
@@ -0,0 +1,3 @@
+0.4.5a (2014-05-13)
+-------------------
+* Initial release with libsodium 0.4.5
--- lib/rbnacl/libsodium/version.rb
+++ lib/rbnacl/libsodium/version.rb
@@ -1,5 +1,5 @@
 module RbNaCl
   module Libsodium
-    VERSION = "0.0.2"
+    VERSION = "0.4.5a"
   end
 end
