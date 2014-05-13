1862cf03f5d02dbdd1273929785c3bc989e07a83
Artiom Di <kron82@gmail.com>
2014-05-13 16:32:42 +0300

    JRuby support

--- lib/rbnacl/libsodium.rb
+++ lib/rbnacl/libsodium.rb
@@ -3,7 +3,7 @@ require "rbnacl/libsodium/version"
 module RbNaCl
   module Libsodium
     sodiumlib_dir = File.expand_path(File.join(File.dirname(__FILE__), "../../vendor/libsodium/dist/lib/"))
-    sodiumlib_filename = RUBY_PLATFORM =~ /darwin/ ? "libsodium.dylib" : "libsodium.so"
+    sodiumlib_filename = RUBY_DESCRIPTION =~ /darwin/ ? "libsodium.dylib" : "libsodium.so"
     ::RBNACL_LIBSODIUM_GEM_LIB_PATH = File.join(sodiumlib_dir, sodiumlib_filename)
   end
 end
