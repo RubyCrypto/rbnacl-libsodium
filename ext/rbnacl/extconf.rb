83dec04fbdd6822d3ccdea6c377e2f0c3f8ac78b
Hiroshi Shirosaki <h.shirosaki@gmail.com>
2016-04-27 11:53:44 +0900

    Build Windows binary packages
    We can cross compile Windows binary on docker using
    rake-compiler-dock and rake-compiler.
    `rake gem:windows` will pull docker image, cross compile and
    create the following packages in `pkg` directory.
    - rbnacl-libsodium-x.x.x-x64-mingw32.gem
    - rbnacl-libsodium-x.x.x-x86-mingw32.gem

--- ext/rbnacl/extconf.rb
+++ ext/rbnacl/extconf.rb
@@ -1,3 +1,5 @@
+require 'mkmf'
+
 def sys(cmd)
   puts " -- #{cmd}"
   unless ret = system(cmd)
@@ -14,13 +16,26 @@ clean:
 \t@echo "Nothing to do"
 MAKEFILE
 
+HOST = arg_config("--host")
+OPTIONS = []
+if HOST
+  OPTIONS << "--host=#{HOST}"
+end
+
 CWD = File.expand_path(File.dirname(__FILE__))
 LIBSODIUM_DIR = File.expand_path(File.join(CWD, '..', '..', 'vendor', 'libsodium'))
 MAKE = ENV['MAKE'] || ENV['make'] || "make"
+if HOST
+  # install to the stage directory for rake-compiler
+  DIST = File.expand_path(File.join(CWD, '..', '..', 'tmp', RUBY_PLATFORM, 'stage', 'vendor', 'libsodium', 'dist'))
+else
+  DIST = "#{LIBSODIUM_DIR}/dist"
+end
 
 Dir.chdir(LIBSODIUM_DIR) do
   # sh is required to run configure on Windows
-  sys("sh -c \"./configure --prefix=#{LIBSODIUM_DIR}/dist\"")
+  sys("sh -c \"./configure --prefix=#{DIST} #{OPTIONS.join(' ')}\"")
+  sys("#{MAKE} clean") if HOST
   sys(MAKE)
   sys("#{MAKE} install")
 end
